80dd80c1ccb2c607eecd3ca715150e0bca64af43
Kyutoku <github@kyutoku.cloud>
2023-01-14 00:37:12 +0900

    vlp16 fix

--- nebula_offline_sample/include/velodyne/velodyne_ros_offline_extract_bag_pcd.hpp
+++ nebula_offline_sample/include/velodyne/velodyne_ros_offline_extract_bag_pcd.hpp
@@ -1,17 +1,16 @@
 #ifndef NEBULA_VelodyneRosOfflineExtractBag_H
 #define NEBULA_VelodyneRosOfflineExtractBag_H
 
+#include <ament_index_cpp/get_package_prefix.hpp>
+#include <diagnostic_updater/diagnostic_updater.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+
 #include "common/nebula_common.hpp"
 #include "common/nebula_driver_ros_wrapper_base.hpp"
 #include "common/nebula_status.hpp"
 #include "velodyne/velodyne_common.hpp"
 #include "velodyne/velodyne_driver.hpp"
-
-#include <diagnostic_updater/diagnostic_updater.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_components/register_node_macro.hpp>
-#include <ament_index_cpp/get_package_prefix.hpp>
-
 #include "velodyne_msgs/msg/velodyne_packet.hpp"
 #include "velodyne_msgs/msg/velodyne_scan.hpp"
 
@@ -23,8 +22,8 @@ class VelodyneRosOfflineExtractBag final : public rclcpp::Node, NebulaDriverRosW
 {
   std::shared_ptr<drivers::VelodyneDriver> driver_ptr_;
   Status wrapper_status_;
-//  rclcpp::Subscription<velodyne_msgs::msg::VelodyneScan>::SharedPtr velodyne_scan_sub_;
-//  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_pub_;
+  //  rclcpp::Subscription<velodyne_msgs::msg::VelodyneScan>::SharedPtr velodyne_scan_sub_;
+  //  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_pub_;
 
   std::shared_ptr<drivers::CalibrationConfigurationBase> calibration_cfg_ptr_;
   std::shared_ptr<drivers::SensorConfigurationBase> sensor_cfg_ptr_;
@@ -49,12 +48,15 @@ public:
 
   Status GetStatus();
   Status ReadBag();
+
 private:
   std::string bag_path;
   std::string storage_id;
   std::string out_path;
   std::string format;
   std::string target_topic;
+  int out_num;
+  int skip_num;
 };
 
 }  // namespace ros
