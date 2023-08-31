01d12a018faa4419cca939c78cff2a1dc010d101
amc-nu <abraham.monrroy@gmail.com>
2023-08-31 18:04:02 +0900

    remove comments
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>

--- nebula_ros/src/hesai/hesai_decoder_ros_wrapper.cpp
+++ nebula_ros/src/hesai/hesai_decoder_ros_wrapper.cpp
@@ -57,7 +57,7 @@ void HesaiDriverRosWrapper::ReceiveScanMsgCallback(
   const pandar_msgs::msg::PandarScan::SharedPtr scan_msg)
 {
   auto t_start = std::chrono::high_resolution_clock::now();
-
+  RCLCPP_INFO_STREAM(get_logger(), scan_msg->packets.size());
   std::tuple<nebula::drivers::NebulaPointCloudPtr, double> pointcloud_ts =
     driver_ptr_->ConvertScanToPointcloud(scan_msg);
   nebula::drivers::NebulaPointCloudPtr pointcloud = std::get<0>(pointcloud_ts);
